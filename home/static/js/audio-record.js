bed769d6f58815a6ee1503d67a79197693fbb673
kudo313 <kudo3139a@gmail.com>
2021-09-17 18:16:28 +0700

    record animtion

--- home/static/js/audio-record.js
+++ home/static/js/audio-record.js
@@ -133,3 +133,17 @@ function createDownloadLink(blob) {
     
 	//add the li element to the ol
 }
+
+// recording animation
+$('#record-button').addClass("notRec");
+
+$('#record-button').click(function(){
+	if($('#record-button').hasClass('notRec')){
+		$('#record-button').removeClass("notRec");
+		$('#record-button').addClass("Rec");
+	}
+	else{
+		$('#record-button').removeClass("Rec");
+		$('#record-button').addClass("notRec");
+	}
+});	
